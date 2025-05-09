9f7f80bd56d17bddda1c2e768e947e634a87d5d8
Jean-Georges "jgp" Perrin <jgperrin@users.noreply.github.com>
2025-05-09 11:20:24 -0400

    Cleanup

--- chapter-02/resources/customer.sql
+++ chapter-02/resources/customer.sql
@@ -1,13 +1,21 @@
+-- Table: Customer
 CREATE TABLE Customer (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR(20)
 );
 
+-- Table: AddressType
+CREATE TABLE AddressType (
+    address_type_id SERIAL PRIMARY KEY,
+    address_type VARCHAR(20) NOT NULL
+);
+
+-- Table: Address
 CREATE TABLE Address (
-    address_id INT PRIMARY KEY AUTO_INCREMENT,
+    address_id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL,
     street1 VARCHAR(255) NOT NULL,
     street2 VARCHAR(255),
@@ -15,6 +23,8 @@ CREATE TABLE Address (
     state VARCHAR(100),
     postal_code VARCHAR(20),
     country_cd CHAR(2) NOT NULL,
-    address_type ENUM('Billing', 'Shipping') NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id) ON DELETE CASCADE
+    address_type_id INT,
+
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customer(customer_id) ON DELETE CASCADE,
+    CONSTRAINT fk_address_type FOREIGN KEY (address_type_id) REFERENCES AddressType(address_type_id)
 );
